4855a433510872ecda6dbade26ad04e60d36cc3c
Teerachot Mueangchamnong <61083406+M-TRCH@users.noreply.github.com>
2025-09-09 09:59:36 +0700

    Add firmware_v1.0.0

--- include/config.h
+++ include/config.h
@@ -12,7 +12,7 @@
 #define DEFAULT_FW_VERSION      28085   // ddmmy dd=day, mm=month, y=year (e.g. 28085 = 28/08/2025)
 #define DEFAULT_HW_VERSION      401     // mnp m=major, n=minor, p=production time
 #define DEFAULT_BAUD_RATE       9600
-#define DEFAULT_IDENTIFIER      99      // for Testing
+#define DEFAULT_IDENTIFIER      247     // Default Modbus ID (1-246, 247=not set)
 #define DEFAULT_LED_BRIGHTNESS  {20, 20, 20, 20, 20, 20, 20, 20}
 #define DEFAULT_LED_R           {255,   0,      0,      255,    0,      255,    255,    255}
 #define DEFAULT_LED_G           {0,     255,    0,      255,    255,    0,      128,    255}

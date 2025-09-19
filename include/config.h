83408b5dec82946952f23f95362ca64adb70ecd2
Teerachot Mueangchamnong <61083406+M-TRCH@users.noreply.github.com>
2025-09-19 11:13:18 +0700

    Update default configuration values for device type, firmware version, and LED settings

--- include/config.h
+++ include/config.h
@@ -8,14 +8,14 @@
 #define MODBUS_OUTPUT       MODBUS_SERIAL3  // Select the physical output
 
 // Default configuration values
-#define DEFAULT_DEVICE_TYPE     0
-#define DEFAULT_FW_VERSION      10095   // ddmmy dd=day, mm=month, y=year (e.g. 28085 = 28/08/2025)
+#define DEFAULT_DEVICE_TYPE     10      // 10=standard, 20=narcotic
+#define DEFAULT_FW_VERSION      19095   // ddmmy dd=day, mm=month, y=year (e.g. 28085 = 28/08/2025)
 #define DEFAULT_HW_VERSION      401     // mnp m=major, n=minor, p=production time
 #define DEFAULT_BAUD_RATE       9600
 #define DEFAULT_IDENTIFIER      247     // Default Modbus ID (1-246, 247=not set)
-#define DEFAULT_LED_BRIGHTNESS  {90, 90, 90, 90, 90, 90, 90, 90}
+#define DEFAULT_LED_BRIGHTNESS  {80, 80, 80, 80, 80, 80, 80, 80}
 #define DEFAULT_LED_R           {255,   0,      0,      255,    0,      255,    255,    255}
-#define DEFAULT_LED_G           {0,     255,    0,      255,    255,    0,      128,    255}
-#define DEFAULT_LED_B           {0,     0,      255,    0,      255,    255,    0,      235}
+#define DEFAULT_LED_G           {0,     255,    0,      215,    255,    0,      60,     245}
+#define DEFAULT_LED_B           {0,     0,      255,    0,      255,    255,    0,      120}
 
 #endif
